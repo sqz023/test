bc189f9a61d7286f5fba065dd965fc9f202a937d
vagrant <vagrant@localhost.localdomain>
2017-11-27 00:08:48 +0000

    this is the first ever commit

---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: cookbooks
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
---
+++ recipes/setup.rb
@@ -0,0 +1,11 @@
+package 'tree' do
+ action :install
+end
+
+package 'git' do
+ action	:install
+end
+
+package 'ntp' do
+ action :install
+end
